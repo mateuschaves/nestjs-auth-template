84d447414dfb4c921ee7bea0499c58124afecaf9
Mateus Henrique <mateushenriquechaves1@gmail.com>
2021-07-22 03:20:18 -0300

    auth module

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:16.2-alpine
+FROM node:16.4
 
 WORKDIR /usr/app
 COPY ./package.json ./yarn.lock ./
---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOSTNAME,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: true,
+};
